bc55e193e894c497e385dbaa8ef3afafd6de8a72
Baptiste Domange <baptiste.domange@gmail.com>
2022-01-12 00:18:07 +0100

    player death, jump on kill, background

--- src/Actors/Enemy.gd
+++ src/Actors/Enemy.gd
@@ -5,8 +5,16 @@ func _ready():
 	set_physics_process(false) # deactivate the enemy at start of the game
 	_velocity.x = -speed.x # go to the left
 
+# callback function generate from Node > Signals
+func _on_StompDetector_body_entered(body: PhysicsBody2D):
+	if body.global_position.y > get_node("StompDetector").global_position.y:
+		return 
+	get_node("CollisionShape2D").disabled = true
+	queue_free()
+
 func _physics_process(delta):
 	_velocity.y += gravity * delta
 	if is_on_wall():
 		_velocity.x *= -1.0
 	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
+
--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -1,10 +1,18 @@
 extends Actor
 
+export var stomp_impulse: = 1000.0
+
 func _physics_process(delta: float) -> void:
 	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	var direction: = get_direction()
 	_velocity = compute_velocity(_velocity, direction, speed, is_jump_interrupted)
 	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
+
+func _on_EnemyDetector_area_entered(area: Area2D):
+	_velocity = compute_stomp_velocity(_velocity, stomp_impulse)
+
+func _on_EnemyDetector_body_entered(body: PhysicsBody2D):
+	queue_free()
 
 func get_direction() -> Vector2:
 	return Vector2(
@@ -25,4 +33,9 @@ func compute_velocity(
 		out.y = speed.y * direction.y
 	if is_jump_interrupted:
 		out.y = 0.0
+	return out
+
+func compute_stomp_velocity(linear_velocity: Vector2, 	impulse: float) -> Vector2:
+	var out: = linear_velocity
+	out.y = -impulse
 	return out
