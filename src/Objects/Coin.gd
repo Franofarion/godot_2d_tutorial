9122e914cab5c7e023aef303cec6c0c390f7d0e3
Baptiste Domange <baptiste.domange@gmail.com>
2022-01-13 09:30:15 +0100

    coin animation

--- src/Objects/Coin.gd
+++ src/Objects/Coin.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-onready var anim_player: AnimationPlayer = get_node("AnimationPlayer")
+onready var anim_player: AnimationPlayer = get_node("coin/AnimationPlayer")
 
 func _on_body_entered(body):
 	anim_player.play("fade_out")
